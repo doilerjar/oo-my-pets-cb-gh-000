59b595dabdb9385145d332fad72b20ca1c2404e5
Kevin K <doilerjar@outlook.com>
2018-08-11 08:39:35 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,9 @@ class Owner
   end
   
   def sell_pets
-    @pets = { :fishes=>[], :dogs=>[], :cats=>[] }
+    @pets[:dogs].each {|dog| dog.mood = "nervous"}
+    @pets[:cats].each {|cat| cat.mood = "nervous"}
+    @pets[:fishes].each {|fish| fish.mood = "nervous"}
   end 
   
   def list_pets
