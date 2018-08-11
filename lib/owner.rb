d74226c18f9310ef3de17aaad7ddf0f98b4d10a8
Kevin K <doilerjar@outlook.com>
2018-08-11 08:07:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,5 +2,23 @@ class Owner
   # code goes here
   attr_accessor :pets
   
+  attr_reader :species
+  
+  @@all = []
+  
+  def initialize(species="")
+    @species = species  
+  end
+  
+  def self.reset_all
+    
+  end
+  
+  def 
+  
+  def say_species
+    @species
+  end
+  
   
 end
